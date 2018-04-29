5fc3b6e650f7f7a19eb85e43fb89e4e307c3f300
marth8880 <marth8880@gmail.com>
2018-04-29 14:00:12 -0700

    - Add functionality to convert a localized key value into a readable string; - Further attempts to fix Scope nesting;

--- SWBF2-Localization-Parser/Program.cs
+++ SWBF2-Localization-Parser/Program.cs
@@ -12,11 +12,20 @@ namespace SWBF2_Localization_Parser
 	{
 		static void Main(string[] args)
 		{
-			string path = @"J:\BF2_ModTools\data_LCT\Common\Localize\english.cfg";
+			string path = @"J:\BF2_ModTools\data_LCT\Common\english2.cfg";
 			string path2 = @"J:\BF2_ModTools\data_TCW\data_TCW\Common\Localize\english.cfg";
 
-			DataBase db = new DataBase();
-			db = ParseDataBase(path2);
+			//DataBase db = new DataBase();
+			//db = ParseDataBase(path);
+
+			List<string> testList = new List<string>();
+			testList.Add("00000000450034007500F500440056006700F500240057009600C6004600F500");
+			testList.Add("33000300230013001300F20003001300D000A00005002500F400050054002500");
+			testList.Add("450095000200F400640002006400250014009500540044000200750094002500");
+			testList.Add("54003500020035004500550044009400F4003500D000A0004400F4000200E400");
+			testList.Add("F400450002004400940035004500250094002400550045005400");
+
+			Console.WriteLine(StringExt.ConvertUnicodeListToString(testList));
 
 			// Exit the application
 			Console.WriteLine("\n" + "Press any key to exit.");
@@ -40,7 +49,8 @@ namespace SWBF2_Localization_Parser
 			// Iteration vars
 			Chunk curParentChunk = Chunk.DataBase;
 			List<Chunk> parentChunks = new List<Chunk>();
-			Scope lastScope = new Scope();
+			Scope curLastScope = new Scope();
+			List<Scope> lastScopes = new List<Scope>();
 			Key lastKey = new Key();
 			int curIndex = 0;
 
@@ -66,18 +76,21 @@ namespace SWBF2_Localization_Parser
 
 						// Get a ref to the most recent Scope
 						curIndex = db.Scopes.Count - 1;
-						lastScope = db.Scopes[curIndex];
+						//curLastScope = new Scope();
+						curLastScope = db.Scopes[curIndex];
 					}
 					else if (parentChunks.Last() == Chunk.VarScope)
 					{
-						if (lastScope != null)
+						if (curLastScope != null)
 						{
 							// Add the new Scope to the DataBase container
-							lastScope.Scopes.Add(scope);
+							curLastScope.Scopes.Add(scope);
 
 							// Get a ref to the most recent Scope
-							curIndex = lastScope.Scopes.Count - 1;
-							lastScope = lastScope.Scopes[curIndex];
+							curIndex = curLastScope.Scopes.Count - 1;
+							//curLastScope = new Scope();
+							curLastScope = curLastScope.Scopes[curIndex];
+							//lastScopes.Add(curLastScope);
 						}
 					}
 
@@ -105,11 +118,11 @@ namespace SWBF2_Localization_Parser
 						if (lastKey != null)
 						{
 							// Add the new Key to the Scope container
-							lastScope.Keys.Add(key);
+							curLastScope.Keys.Add(key);
 
 							// Get a ref to the most recent Key
-							curIndex = lastScope.Keys.Count - 1;
-							lastKey = lastScope.Keys[curIndex];
+							curIndex = curLastScope.Keys.Count - 1;
+							lastKey = curLastScope.Keys[curIndex];
 						}
 					}
 
@@ -122,19 +135,24 @@ namespace SWBF2_Localization_Parser
 				// Is this the beginning of a new chunk?
 				else if (curLine.Contains("{"))
 				{
+					if (curParentChunk == Chunk.VarScope)
+					{
+						lastScopes.Add(curLastScope);
+						//curLastScope = new Scope();
+					}
 					parentChunks.Add(curParentChunk);
 				}
 				// Is this the end of the current chunk?
 				else if (curLine.Contains("}"))
 				{
-					if (parentChunks.Count > 2)
+					if (curParentChunk == Chunk.VarScope)
 					{
-						if (parentChunks[parentChunks.Count - 1] == Chunk.VarScope && parentChunks[parentChunks.Count - 2] == Chunk.VarScope)
-						{
-							lastScope = new Scope();
-						}
+						lastScopes.RemoveAt(lastScopes.Count - 1);
 					}
+					else if (curParentChunk == Chunk.VarBinary)
+					{
 
+					}
 					parentChunks.RemoveAt(parentChunks.Count - 1);
 				}
 				// If we're in a VarBinary chunk, parse the Key properties
@@ -217,10 +235,40 @@ namespace SWBF2_Localization_Parser
 			if (value == null) throw new ArgumentNullException();
 			if (endIndex > value.Length) throw new IndexOutOfRangeException("End index must be less than or equal to the length of the string.");
 			if (startIndex < 0 || startIndex > value.Length + 1) throw new IndexOutOfRangeException("Start index must be between zero and the length of the string minus one");
-			if (startIndex >= endIndex) throw new ArgumentOutOfRangeException("Start index must be less then end index");
+			if (startIndex >= endIndex) throw new ArgumentOutOfRangeException("Start index must be less than end index");
 
 			var length = endIndex - startIndex;
 			return value.Substring(startIndex, length);
+		}
+
+		public static string Reverse(string s)
+		{
+			char[] charArray = s.ToCharArray();
+			Array.Reverse(charArray);
+			return new string(charArray);
+		}
+
+		public static string ConvertUnicodeListToString(List<string> s)
+		{
+			string combinedStr = String.Concat(s);
+			string[] valueArray = Split(combinedStr, 4).ToArray();
+			string[] reversedValues = new string[valueArray.Length];
+			char[] chars = new char[valueArray.Length];
+
+			// Go through the list of values, then reverse them and convert them into their character representations
+			for (int i = 0; i < valueArray.Length; i++)
+			{
+				reversedValues[i] = Reverse(valueArray[i]);
+				chars[i] = (char)int.Parse(reversedValues[i], System.Globalization.NumberStyles.HexNumber);
+			}
+
+			return new string(chars);
+		}
+
+		public static IEnumerable<string> Split(string str, int chunkSize)
+		{
+			return Enumerable.Range(0, str.Length / chunkSize)
+				.Select(i => str.Substring(i * chunkSize, chunkSize));
 		}
 	}
 
